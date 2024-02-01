ebb7a4d7abe0ea6f5571278b90a075b6f08df824
danielzapatta <vivadanielzapata@hotmail.com>
2024-02-01 14:39:46 -0300

    Service layer and component registration

--- src/main/java/com/educandoweb/course/repositories/UserRepository.java
+++ src/main/java/com/educandoweb/course/repositories/UserRepository.java
@@ -4,6 +4,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.educandoweb.course.entities.User;
 
+// Já está herdando do JPA Repository que é um componente do Spring, não há necessidade de Annotations
 public interface UserRepository extends JpaRepository<User, Long>{
 
 }
